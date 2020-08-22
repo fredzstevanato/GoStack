10ad0261ed732052749e56024bb04239d195e43b
Fredercio Stevanato Rocha <fredzstevanato@gmail.com>
2020-08-21 20:53:39 -0400

    repositorio unico para desafios

---
+++ desafio-02/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(repository => {
+      setRepositories(repository.data);
+    });
+  },[]);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'Teste',
+      url: 'https://github.com/fredzstevanato',
+      techs: ["NodeJS","ReactJS", "React Native"]
+    });
+    console.log(response.data)
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id));
+  }
+  return (
+    <div>
+      <ul data-testid="repository-list">
+      {repositories.map(repository => (
+              <li key={repository.id}>
+                {repository.title}
+
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
